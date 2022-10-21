3e0c9b162a3033e7e39f87a61f26ba485c1af450
Peam <peammy2010@hotmail.com>
2022-10-21 13:59:09 +0700

    🗒

--- src/controllers/student_bio.controller.ts
+++ src/controllers/student_bio.controller.ts
@@ -1,33 +1,8 @@
-import { prisma } from '../common/prisma/prisma'
 import { Request, Response } from 'express'
-import { BioDto, UpdateBioDto } from '../dto/student_bio.dto'
 
-const getOneStudentBio = async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id)
-  const bio = await prisma.studentBio.findUnique({ where: { studentId: id } })
+const getOneStudentBio = async (req: Request, res: Response) => { }
 
-  if (bio === null) return res.status(404).json({ message: 'not found' })
-
-  const studentBioDto: BioDto = {
-    id: bio.id,
-    birthdate: bio.birthdate,
-    highSchoolName: bio.highSchoolName,
-  }
-  res.status(200).json(studentBioDto)
-}
-
-const updateStudentBio = async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id)
-  const newBio: UpdateBioDto = req.body
-  const bio = await prisma.studentBio.update({
-    where: { id },
-    data: {
-      birthdate: newBio.birthdate,
-      highSchoolName: newBio.highSchoolName,
-    }
-  })
-  res.status(200).json(bio)
-}
+const updateStudentBio = async (req: Request, res: Response) => { }
 
 export {
   getOneStudentBio,
